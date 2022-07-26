f2f1500a57fc0db66ea859569e2759c5c2770360
ungerecht <kevinungerecht@gmail.com>
2022-07-26 04:37:34 -0700

    Use Chakra, EntryForm

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+
+import { ChakraProvider, Container, Heading } from '@chakra-ui/react'
+
 import { io } from 'socket.io-client'
+
+import EntryForm from './EntryForm'
 
 const App = () => {
   //   const socket = io('http://localhost:8080')
   return (
-    <div className='App'>
-      <header className='App-header'></header>
-    </div>
+    <ChakraProvider>
+      <div className='App'>
+        <Container>
+          <Heading textAlign='center'>Six Card Golf</Heading>
+          <EntryForm />
+        </Container>
+      </div>
+    </ChakraProvider>
   )
 }
 
