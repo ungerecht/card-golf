59d9a6287d79fd159376b8e9037d0e2465a2c5f4
ungerecht <kevinungerecht@gmail.com>
2022-08-10 21:11:21 -0700

    Create createGame and joinGame methods

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 
 import { ChakraProvider, Container, Heading } from '@chakra-ui/react'
 
-import { io } from 'socket.io-client'
+import { io, Socket } from 'socket.io-client'
 
 import EntryForm from './EntryForm'
 
 const App = () => {
-  //   const socket = io('http://localhost:8080')
+  const socket = io('http://localhost:8080')
+
+  const createGame = (numHoles: number) => {
+    console.log('create game with ' + numHoles + ' holes')
+    socket.emit('createRoom', (socket: Socket) => {
+      console.log(socket)
+    })
+  }
+
+  const joinGame = (roomCode: string) => {
+    console.log('join game ' + roomCode)
+  }
+
   return (
     <ChakraProvider>
       <div className='App'>
@@ -15,7 +27,7 @@ const App = () => {
           <Heading textAlign='center' mb='20'>
             Six Card Golf
           </Heading>
-          <EntryForm />
+          <EntryForm createGame={createGame} joinGame={joinGame} />
         </Container>
       </div>
     </ChakraProvider>
