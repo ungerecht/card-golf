0b7bcbd96a36b3a253aa0130d497980945a5f879
ungerecht <kevinungerecht@gmail.com>
2022-08-11 16:21:04 -0700

    Add room listeners

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const { nanoid } = require('nanoid') //uuid generator for rooms
+
 const express = require('express')
 const app = express()
 
@@ -14,15 +16,36 @@ const io = new Server(server, {
   },
 })
 
-const uuid = require('uuid-random')
-
 const PORT = 8080 || process.env.PORT
 
 const rooms = {}
 
 io.on('connection', (socket) => {
-  socket.id = uuid()
-  console.log('new connection')
+  console.log('new connection ' + socket.id)
+
+  socket.on('createRoom', (playerName, numHoles) => {
+    console.log(playerName + ' is creating room with ' + numHoles)
+
+    //make new room
+    const room = {
+      id: nanoid(8),
+      sockets: [],
+      numHoles: numHoles,
+    }
+    rooms[room.id] = room
+
+    //tell the client a room was created
+    socket.emit('roomCreated', playerName, room.id)
+  })
+
+  socket.on('joinRoom', (playerName, roomId) => {
+    console.log(playerName + ' joining room ' + roomId)
+
+    //join room
+    socket.playerName = playerName
+    rooms[roomId].sockets.push(socket)
+    socket.join(roomId)
+  })
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
