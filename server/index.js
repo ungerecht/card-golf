be8da37634d237d5233abe421043d8edbdcd090e
ungerecht <kevinungerecht@gmail.com>
2022-08-17 23:53:18 -0700

    Add listeners for checkRoom, checkPassword, and leaveRoom

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const rooms = {}
 io.on('connection', (socket) => {
   console.log('new connection ' + socket.id)
 
-  socket.on('createRoom', (playerName, numHoles) => {
+  socket.on('createRoom', (playerName, numHoles, password) => {
     console.log(playerName + ' is creating room with ' + numHoles)
 
     //make new room
@@ -31,20 +31,46 @@ io.on('connection', (socket) => {
       id: nanoid(8),
       sockets: [],
       numHoles: numHoles,
+      password: password,
     }
     rooms[room.id] = room
 
     //tell the client a room was created
-    socket.emit('roomCreated', playerName, room.id)
+    socket.emit('roomCreated', playerName, room.id, password)
+  })
+
+  socket.on('checkRoom', async (roomId, callback) => {
+    const isRoom = rooms[roomId] !== undefined
+    const hasPassword = isRoom ? rooms[roomId].password.length > 0 : false
+    callback({ isRoom, hasPassword })
+  })
+
+  socket.on('checkPassword', async (roomId, password, callback) => {
+    const correct = rooms[roomId].password === password
+    callback({ correct })
   })
 
   socket.on('joinRoom', (playerName, roomId) => {
     console.log(playerName + ' joining room ' + roomId)
 
-    //join room
-    socket.playerName = playerName
-    rooms[roomId].sockets.push(socket)
-    socket.join(roomId)
+    if (rooms[roomId].sockets.length < 6) {
+      //max of 6 players per room
+      //join room
+      socket.playerName = playerName
+      rooms[roomId].sockets.push(socket)
+      socket.join(roomId)
+    } else {
+      socket.emit('fullRoom')
+    }
+  })
+
+  socket.on('leaveRoom', (playerName, roomId) => {
+    console.log(playerName + ' leaving room ' + roomId)
+
+    rooms[roomId].sockets = rooms[roomId].sockets.filter(
+      (item) => item != socket
+    )
+    socket.leave(roomId)
   })
 
   socket.on('disconnect', () => {
