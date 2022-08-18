57861d858ca6ac7461a19f839bfc0e8cfddd3e3b
ungerecht <kevinungerecht@gmail.com>
2022-08-17 23:55:10 -0700

    Add events for checkRoom, checkPassword

--- client/src/features/socket.ts
+++ client/src/features/socket.ts
@@ -1,23 +1,57 @@
 import { io } from 'socket.io-client'
+import { checkPasswordResponse, checkRoomResponse } from '../types'
 
 const socket = io('http://localhost:8080')
 
-export const createRoom = (playerName: string, numHoles: number) => {
+export const createRoom = async (
+  playerName: string,
+  numHoles: number,
+  password: string
+) => {
   console.log(playerName + ' is creating room with ' + numHoles + ' holes')
 
-  socket.emit('createRoom', playerName, numHoles)
+  await socket.emit('createRoom', playerName, numHoles, password)
 }
 
-export const joinRoom = (playerName: string, roomId: string) => {
+export const checkRoom = (roomId: string) => {
+  return new Promise((resolve: (value: checkRoomResponse) => void) => {
+    socket.emit('checkRoom', roomId, (response: checkRoomResponse) => {
+      resolve(response)
+    })
+  })
+}
+
+export const checkPassword = async (roomId: string, password: string) => {
+  return new Promise((resolve: (value: checkPasswordResponse) => void) => {
+    socket.emit(
+      'checkPassword',
+      roomId,
+      password,
+      (response: checkPasswordResponse) => {
+        resolve(response)
+      }
+    )
+  })
+}
+
+export const joinRoom = async (playerName: string, roomId: string) => {
   console.log('Joining room ' + roomId)
-  socket.emit('joinRoom', playerName, roomId)
+
+  await socket.emit('joinRoom', playerName, roomId)
 }
 
-export const leaveRoom = () => {
+export const leaveRoom = async () => {
   console.log('Leaving room')
+
+  await socket.emit('leaveRoom')
 }
 
 //When room is created, join the room
 socket.on('roomCreated', (playerName: string, roomId: string) => {
+  console.log('Room ' + roomId + ' created.')
   joinRoom(playerName, roomId)
+})
+
+socket.on('fullRoom', () => {
+  console.log('That room is full')
 })
